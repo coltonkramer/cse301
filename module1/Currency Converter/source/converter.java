ca6d7df6a7e94d036e4a8ed88a989f6d8e71828d
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-10-02 11:44:14 -0400

    Finished module 1

---
+++ module1/Currency Converter/source/converter.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class converter {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Currency Converter");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter which currency you will be using (USD,EUR,JPY,CNY,RUB)");
+		
+		String currency1 = input.nextLine();
+		System.out.println("Currency is " + currency1);
+		
+		System.out.println("Enter the amount you would like to convert");
+		String money = input.nextLine();
+
+		System.out.println("Enter the currency that it will be converted to (USD,EUR,JPY,CNY,RUB)");
+		String currency2 = input.nextLine();
+		input.close();
+
+		double newAmount = Double.parseDouble(money);
+
+
+		
+
+		String symbol1 = "";
+		String symbol2 = "";
+
+		switch(currency1.toUpperCase()){
+			case "USD":
+			symbol1 = "$";
+			break;
+			case "EUR":
+			symbol1 = "€";
+			newAmount = newAmount * 1.15871;
+			break;
+			case "JPY":
+			symbol1 = "¥";
+			newAmount = newAmount * 0.009;
+			break;
+			case "CNY":
+			symbol1 = "¥";
+			newAmount = newAmount * 0.15507;
+			break;
+			case "RUB":
+			symbol1 = "₽";
+			newAmount = newAmount * 0.01372;
+			break;
+		}
+
+		switch(currency2.toUpperCase()){
+			case "USD":
+			symbol2 = "$";
+			break;
+			case "EUR":
+			symbol2 = "€";
+			newAmount = newAmount * 0.86292;
+			break;
+			case "JPY":
+			symbol2 = "¥";
+			newAmount = newAmount * 111.12;
+			break;
+			case "CNY":
+			symbol2 = "¥";
+			newAmount = newAmount * 6.44468;
+			break;
+			case "RUB":
+			symbol2 = "₽";
+			newAmount = newAmount * 72.8191;
+			break;
+		
+		}
+
+		System.out.println("You want to convert " + symbol1 + money + " " + currency1.toUpperCase() + " to " + currency2.toUpperCase());
+
+		DecimalFormat round = new DecimalFormat("0.00");
+		String roundedNewAmount = round.format(newAmount);
+		System.out.printf(symbol1 + money + currency1.toUpperCase() +  " = " + symbol2 + roundedNewAmount + currency2.toUpperCase());
+		
+
+
+	}
+
+		
+
+		
+
+}
+	
+
---
+++ module1/README.md
@@ -0,0 +1,14 @@
+#Currency Converter
+## Author: Colton Kramer
+### Module 1 CSE301 Language - Java
+### Instructor: Brother Pineda
+***
+The purpose of this project was to learn something new on my own. I chose to learn a little bit about Java.
+I built a simple currency converter that runs in the command line. I look into creating an interface for it but with the time given
+for the project, it seemed a little ambitious.
+
+The program will take a currency code ex. USD and convert it into a currency of your choosing. To eliminate hard coding every conversion
+I simply use a switch block to convert everything to USD then a following switch block that converts it into the final currency. 
+
+Thank you for taking a look at my work!
+***
