72ed4ad71e34572d9a52c4e77ea32a5df3f7d019
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-10-02 14:00:34 -0400

    added comments to my code

--- module1/Currency Converter/source/converter.java
+++ module1/Currency Converter/source/converter.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
+//Title: Currency Converter
+//Author: Colton Kramer
+//Description: Converts 5 common currencies between eachother.
+ 
 
 public class converter {
-	
-	public static void main(String[] args) {
+
+		public static void main(String[] args) {
 
 			System.out.println("Welcome to the Currency Converter");
 			Scanner input = new Scanner(System.in);
+			//Uses scanner to get inputs from the user on which currency to use
 			System.out.println("Enter which currency you will be using (USD,EUR,JPY,CNY,RUB)");
 			
 			String currency1 = input.nextLine();
 			System.out.println("Currency is " + currency1.toUpperCase());
 			
+			//Uses scanner again but gets the dollar amount
 			System.out.println("Enter the amount you would like to convert");
 			String money = input.nextLine();
 
+			//Gets the currency you want to convert into
 			System.out.println("Enter the currency that it will be converted to (USD,EUR,JPY,CNY,RUB)");
 			String currency2 = input.nextLine();
 			input.close();
-
+			
+			//converts the string dollar amount into a double
 			double newAmount = Double.parseDouble(money);
 
 
@@ -27,6 +35,7 @@ public class converter {
 			String symbol1 = "";
 			String symbol2 = "";
 
+			// converts everything into us dollars. also grabs the symbol for the coresponding currency
 			switch(currency1.toUpperCase()){
 				case "USD":
 				symbol1 = "$";
@@ -49,6 +58,7 @@ public class converter {
 				break;
 			}
 
+			//converts from usd to one of the 4 remaining options
 			switch(currency2.toUpperCase()){
 				case "USD":
 				symbol2 = "$";
@@ -72,12 +82,16 @@ public class converter {
 			
 			}
 
+			//Prints a statement saying what you have chosen
 			System.out.println("You want to convert " + symbol1 + money + " " + currency1.toUpperCase() + " to " + currency2.toUpperCase());
-
+			
+			//Converts the number into decimal format so that it will only display 2 digits after the decimal
 			DecimalFormat round = new DecimalFormat("0.00");
+			//Saves amount to the roundedNewAmount variable
 			String roundedNewAmount = round.format(newAmount);
 			System.out.println("*******************************************************************************************************************\n");
 			System.out.println("Here is your result:");
+			//Prints the result of the conversion along with the original selection
 			System.out.println(symbol1 + money + currency1.toUpperCase() +  " = " + symbol2 + roundedNewAmount + currency2.toUpperCase());
 			System.out.println("\n*******************************************************************************************************************");
 			System.out.println("Thank you for using the Currency Converter. \nHave a nice day!");
