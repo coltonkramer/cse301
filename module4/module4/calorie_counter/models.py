33f00d48024b715fb2b0e77336300352301a6b29
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-13 21:21:49 -0500

    making progress now

--- module4/module4/calorie_counter/models.py
+++ module4/module4/calorie_counter/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 import datetime
 from django.db import models
 from django.utils import timezone
@@ -9,3 +6,6 @@ from django.utils import timezone
 class Foods(models.Model):
      food = models.CharField(default="pizza", max_length=100)
      calories = models.CharField(default="100", max_length=5)
+
+     def __str__(self):
+          return self.food.title()
--- module4/module4/calorie_counter/views.py
+++ module4/module4/calorie_counter/views.py
@@ -2,27 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Foods
 
-posts = [
-    {
-        'food': 'Hot dog',
-        'calories': '151'
-    },
-    {
-        'food': 'Hamburger',
-        'calories': '354'
-    }
-]
-
-# def all_foods(request):
-#     food_list = Foods.objects.all()
-#     return render(request, 'counter/home.html', {'food_list': food_list})
-
 def calorie_counter(request):
-    # food_list = Foods.objects.all()
+    food_list = Foods.objects.all()
     context = {
         'author': 'Colton Kramer',
-        'posts': posts,
-        # 'food_list': food_list
+        'food_list': food_list
     }
     return render(request, 'counter/home.html', context)
 
@@ -30,7 +14,3 @@ def calorie_counter(request):
 
 def about(request):
         return render(request, 'counter/about.html', {'title': 'About'})
-
-        # <!-- {% for food in foods %}
-        # {{food}}
-        # {% endfor %} -->
